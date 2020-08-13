e6030e4bd57698cc1db5e443290c34d707a839c5
daiki <link5105@gmail.com>
2020-08-13 19:24:45 +0900

    お題一覧、回答表示

--- onomato/models.py
+++ onomato/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Odai(models.Model):
+    odai_text = models.CharField(max_length=4)
+    pub_date = models.DateTimeField('date published')
+
+
+class Answer(models.Model):
+    odai = models.ForeignKey(Odai, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=50)
+    pub_date = models.DateTimeField('date published')
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# class User(models.Model):
+#     user_name = models.CharField(max_length=10)
--- onomato/views.py
+++ onomato/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.template import loader
 
-# Create your views here.
+from .models import Odai,Answer
+
+def index(request):
+    latest_answer=Answer.objects.order_by('-pub_date')[:3]
+    context = {
+        'latest_answer': latest_answer,
+    }
+    return render(request, 'onomato/index.html', context)
+
+    # output =', \n'.join([q.odai for q in latest_answer])
+    # return HttpResponse(output)
+
+def odai_index(request):
+    latest_odai=Odai.objects.order_by('-pub_date')[:3]
+    context={'latest_odai': latest_odai}
+    return render(request, 'onomato/odai_index.html', context)
+
+
+def odai_detail(request, odai_id):
+    odai=Odai.objects.get(id=odai_id)
+    # 回答も取得
+    # answers=Odai.
+    return render(request, 'onomato/odai_detail.html',{'odai': odai}) #回答も渡す
