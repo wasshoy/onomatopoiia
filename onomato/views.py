a15b92b01bbac4b6d538e89cbdfa21c7079d166d
A.Arimura <404ari@gmail.com>
2020-08-13 20:49:51 +0900

    [modify] テンプレートファイル修正

--- onomato/views.py
+++ onomato/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 # from django.template import loader
 
-from .models import Odai,Answer
+from .models import Odai, Answer
+
 
 def index(request):
-    latest_answer=Answer.objects.order_by('-pub_date')[:3]
+    latest_answer = Answer.objects.order_by('-pub_date')[:3]
     context = {
         'latest_answer': latest_answer,
     }
@@ -14,14 +15,16 @@ def index(request):
     # output =', \n'.join([q.odai for q in latest_answer])
     # return HttpResponse(output)
 
+
 def odai_index(request):
-    latest_odai=Odai.objects.order_by('-pub_date')[:3]
-    context={'latest_odai': latest_odai}
-    return render(request, 'onomato/odai_index.html', context)
+    latest_odai = Odai.objects.order_by('-pub_date')[:3]
+    context = {'latest_odai': latest_odai}
+    print(context)
+    return render(request, 'onomato/odai.html', context)
 
 
 def odai_detail(request, odai_id):
-    odai=Odai.objects.get(id=odai_id)
+    odai = Odai.objects.get(id=odai_id)
     # 回答も取得
     # answers=Odai.
-    return render(request, 'onomato/odai_detail.html',{'odai': odai}) #回答も渡す
+    return render(request, 'onomato/odai_detail.html', {'odai': odai})  # 回答も渡す
