f781f00e13da0c3a629a911500454bc2c31d4151
A.Arimura <404ari@gmail.com>
2020-08-16 03:39:19 +0900

    [modify] 不要なコメントアウトを削除

--- onomatopoiia/urls.py
+++ onomatopoiia/urls.py
@@ -3,8 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('onomato.urls')),  # TODO: onomato/ を / にして動くようにする
-    # path('onomato/', include('onomato.urls')),
+    path('', include('onomato.urls')),
     path('accounts/', include('django.contrib.auth.urls')),  # 標準ユーザー機能のために追加
     path('accounts/', include('accounts.urls')),
 ]
