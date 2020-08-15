672e7ecc60e043cc2392ef7023e6cd25ff7730e5
A.Arimura <404ari@gmail.com>
2020-08-16 03:41:46 +0900

    [delete] キャッシュ削除

--- onomatopoiia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('onomato.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),  # 標準ユーザー機能のために追加
-    path('accounts/', include('accounts.urls')),
-]
